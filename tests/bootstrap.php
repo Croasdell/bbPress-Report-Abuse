b9715454482818a2fdf70fb9d7862b3835c1e6ba
Ian Croasdell <iancroasdell@gmail.com>
2025-11-12 07:13:11 +0000

    Add basic bootstrap for plugin tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+// Basic bootstrap for plugin tests. For full WP integration tests, use the WP PHPUnit scaffold.
+// This bootstrap is light and intended for unit tests that don't require the entire WP test-suite.
+
+require_once dirname( __DIR__ ) . '/bbpress-report-abuse.php';
+
+// Provide helper mocks if necessary in future tests.
